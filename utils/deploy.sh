b20e94ac95eb9058e1876d1548a41392a89f5954
Matt Belisle <matthew.belisle@workiva.com>
2018-04-28 15:09:28 -0500

    Deploy with nanoseconds

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -5,7 +5,7 @@ set -e
 hugo
 HASH=$(ipfs add -qr public | tail -n 1)
 NS=$(ssh meico@meico.dance "ipfs id -f='<id>'")
-TIMESTAMP=$(date +%s)
+TIMESTAMP=$(date +%s%N)
 ipfs swarm connect /dnsaddr/meico.dance/ipfs/$NS
 ssh meico@meico.dance /bin/bash << EOF
     set -e
@@ -13,6 +13,5 @@ ssh meico@meico.dance /bin/bash << EOF
     rm -rf ~/www
     mv ~/www-$TIMESTAMP ~/www
     ipfs pin add -r $HASH
-    echo 'Publishing ...'
-    ipfs name publish $HASH
+    ipfs name publish $HASH # This takes a while for some reason
 EOF
