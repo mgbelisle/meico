b8f074445471222aac1521d3765f0031e5d52652
Matt Belisle <matthew.belisle@workiva.com>
2018-04-28 10:13:02 -0500

    Better deploy comments

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -2,14 +2,19 @@
 
 set -e
 
-hugo
+echo 'Building ...'
+hugo --quiet
+echo 'Adding to IPFS ...'
 HASH=$(ipfs add -qr public | tail -n 1)
 ssh meico@meico.dance /bin/bash << EOF
     set -e
+    echo 'Getting ...'
     ipfs get -o ~/www-$HASH /ipfs/$HASH
+    echo 'Pinning ...'
     ipfs pin add -r $HASH
-    echo 'Publishing via IPNS. Not sure why this takes so long ...'
+    echo 'Publishing ...'
     ipfs name publish $HASH
+    echo 'Deploying ...'
     [ -e '~/www' ] && mv ~/www ~/www-pre-$HASH
     mv ~/www-$HASH ~/www
     [ -e "~/www-pre-$HASH" ] && rm -r ~/www-pre-$HASH
