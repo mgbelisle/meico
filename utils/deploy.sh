f87d7f0f2303a317f6342b1e68d71a04db063d31
Matt Belisle <matthew.belisle@workiva.com>
2018-04-28 12:25:33 -0500

    Better deploy

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -6,18 +6,14 @@ echo 'Building ...'
 hugo
 echo 'Adding to IPFS ...'
 HASH=$(ipfs add -qr public | tail -n 1)
-ipfs swarm connect /ip6/2604:a880:2:d0::741:7001/tcp/4001/ipfs/QmY3p5zRK9obUETPowTMeU7ceDmyPyQGrVwguWffBb9rMM # TODO: Better address
+ipfs swarm connect /dnsaddr/meico.dance/ipfs/QmY3p5zRK9obUETPowTMeU7ceDmyPyQGrVwguWffBb9rMM
 ssh meico@meico.dance /bin/bash << EOF
     set -e
-    echo 'Getting ...'
-    ipfs get -o ~/www-$HASH /ipfs/$HASH
+    echo 'Getting from IPFS ...'
+    ipfs get -o ~/www /ipfs/$HASH
     echo 'Pinning ...'
     ipfs pin add -r $HASH
     echo 'Publishing ...'
     ipfs name publish $HASH
-    echo 'Deploying ...'
-    [ -e '~/www' ] && mv ~/www ~/www-pre-$HASH
-    mv ~/www-$HASH ~/www
-    [ -e "~/www-pre-$HASH" ] && rm -r ~/www-pre-$HASH
 EOF
 echo Deployed $HASH
