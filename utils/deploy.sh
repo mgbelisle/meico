7d232f56437921b079156eb796560fce63015c32
Matt Belisle <matthew.belisle@workiva.com>
2018-04-28 14:47:22 -0500

    Deploy working nicely

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -2,18 +2,14 @@
 
 set -e
 
-echo 'Building ...'
 hugo
-echo 'Adding to IPFS ...'
 HASH=$(ipfs add -qr public | tail -n 1)
-ipfs swarm connect /dnsaddr/meico.dance/ipfs/QmY3p5zRK9obUETPowTMeU7ceDmyPyQGrVwguWffBb9rMM
+NS=$(ssh meico@meico.dance "ipfs id -f='<id>'")
+ipfs swarm connect /dnsaddr/meico.dance/ipfs/$NS
 ssh meico@meico.dance /bin/bash << EOF
     set -e
-    echo 'Getting from IPFS ...'
     ipfs get -o ~/www /ipfs/$HASH
-    echo 'Pinning ...'
     ipfs pin add -r $HASH
     echo 'Publishing ...'
     ipfs name publish $HASH
 EOF
-echo Deployed $HASH
