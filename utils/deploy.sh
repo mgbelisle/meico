9a25567b9530b93bc8005f89e24e268b5b046d49
Matt Belisle <matthew.belisle@workiva.com>
2018-04-28 15:01:46 -0500

    Fixing deploy

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -5,10 +5,13 @@ set -e
 hugo
 HASH=$(ipfs add -qr public | tail -n 1)
 NS=$(ssh meico@meico.dance "ipfs id -f='<id>'")
+TIMESTAMP=$(date +%s)
 ipfs swarm connect /dnsaddr/meico.dance/ipfs/$NS
 ssh meico@meico.dance /bin/bash << EOF
     set -e
-    ipfs get -o ~/www /ipfs/$HASH
+    ipfs get -o ~/www-$TIMESTAMP /ipfs/$HASH
+    rm -rf ~/www
+    mv ~/www-$TIMESTAMP ~/www
     ipfs pin add -r $HASH
     echo 'Publishing ...'
     ipfs name publish $HASH
