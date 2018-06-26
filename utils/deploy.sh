af4ff9e08fb5e83d40b23884f47517e8051427eb
Matt Belisle <matthew.belisle@workiva.com>
2018-06-26 12:57:51 -0500

    Adding www-deploy

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-go run utils/build/main.go
-HASH=$(ipfs add -qr www | tail -n 1)
+go run utils/build/main.go --out www-deploy
+HASH=$(ipfs add -qr www-deploy | tail -n 1)
 ID=$(ssh meico@meico.dance "ipfs id -f='<id>'")
 TIMESTAMP=$(date +%s)
 ipfs swarm connect /dnsaddr/meico.dance/ipfs/$ID
