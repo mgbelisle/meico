3e4acb9ee481758e2eaaa70bc4a3433a44327bfc
Matt Belisle <matthew.belisle@workiva.com>
2018-10-27 16:52:16 -0500

    Adding Dr. Fleener transcript

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -2,12 +2,12 @@
 
 set -e
 
+ID=$(ssh meico@meico.dance "ipfs id -f='<id>'")
+ipfs swarm connect /ip6/2604:a880:2:d0::741:7001/tcp/4001/ipfs/$ID
+ipfs swarm connect /dnsaddr/meico.dance/ipfs/$ID
 go run utils/build/main.go --out www-deploy
 HASH=$(ipfs add -qr www-deploy | tail -n 1)
-ID=$(ssh meico@meico.dance "ipfs id -f='<id>'")
 TIMESTAMP=$(date +%s)
-ipfs swarm connect /dnsaddr/meico.dance/ipfs/$ID
-ipfs swarm connect /ip6/2604:a880:2:d0::741:7001/tcp/4001/ipfs/$ID
 ssh meico@meico.dance /bin/bash << EOF
     set -e
     ipfs get -o ~/www-$TIMESTAMP /ipfs/$HASH
