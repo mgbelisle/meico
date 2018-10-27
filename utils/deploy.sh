70d46182c68d9fa87c7323b793703d076f74bfc5
Matt Belisle <matthew.belisle@workiva.com>
2018-10-27 15:18:58 -0500

    Moving transcripts

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -7,6 +7,7 @@ HASH=$(ipfs add -qr www-deploy | tail -n 1)
 ID=$(ssh meico@meico.dance "ipfs id -f='<id>'")
 TIMESTAMP=$(date +%s)
 ipfs swarm connect /dnsaddr/meico.dance/ipfs/$ID
+ipfs swarm connect /ip6/2604:a880:2:d0::741:7001/tcp/4001/ipfs/$ID
 ssh meico@meico.dance /bin/bash << EOF
     set -e
     ipfs get -o ~/www-$TIMESTAMP /ipfs/$HASH
