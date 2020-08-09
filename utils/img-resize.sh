a2c4934acf6e099a9510a8d32e4e3ffc1ffabc46
Matt Belisle <matthew.belisle@workiva.com>
2020-08-08 21:10:45 -0500

    Added story lane gallery photos

--- utils/img-resize.sh
+++ utils/img-resize.sh
@@ -4,13 +4,12 @@
 # absolute path to image folder
 FOLDER="$HOME/Desktop"
 
-# max height
+# max width
 WIDTH=1024
 
-# max width
+# max height
 HEIGHT=1024
 
 # resize jpg only to either height or width, keeps proportions using imagemagick
-find ${FOLDER} -iname '*.jpg' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
-find ${FOLDER} -iname '*.jpeg' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
-find ${FOLDER} -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+find $FOLDER -iname '*.jp*g' -exec convert '{}' -verbose -resize $WIDTHx$HEIGHT\> '{}' \;
+find $FOLDER -iname '*.png' -exec convert '{}' -verbose -resize $WIDTHx$HEIGHT\> '{}' \;
