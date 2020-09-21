ff0494f85c307dfeaa5985f760e069e8d8edcec5
Matthew Belisle <matthew.belisle@workiva.com>
2020-09-21 16:48:43 -0500

    sha-rename.py WIP

--- utils/sha-rename.py
+++ utils/sha-rename.py
@@ -5,13 +5,21 @@ import os
 import sys
 
 
+json_file = sys.argv[1]
+photos_dir = sys.argv[2]
 sha1_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'src', 'img', 'sha1')
 
-for root, dnames, fnames in os.walk(sys.argv[1]):
+for root, dnames, fnames in os.walk(photos_dir):
     for fname in fnames:
         base, ext = os.path.splitext(fname)
+        ext = ext.lower()
         with open(os.path.join(root, fname), 'rb') as fhandle:
-            content = fhandle.read()
-        hash = sha1(content).hexdigest()
-        with open(os.path.join(sha1_dir, hash+ext.lower()), 'wb') as fhandle:
-            fhandle.write(content)
+            img = fhandle.read()
+        hash = sha1(img).hexdigest()
+        with open(os.path.join(sha1_dir, hash+ext), 'wb') as fhandle:
+            fhandle.write(img)
+        relpath = os.path.relpath(os.path.join(root, fname), start=photos_dir)
+        with open(json_file) as fhandle:
+            json = fhandle.read()
+        with open(json_file, 'w') as fhandle:
+            fhandle.write(json.replace(f'"{relpath}"', f'"{hash+ext}"'))
