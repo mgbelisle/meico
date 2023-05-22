1879d87c97db3b7671b4349985c9dddbf04d6d96
Matthew Belisle <matthew.belisle@workiva.com>
2023-05-22 06:07:02 -0700

    Moving next to root

--- docs/next/js/main.js
+++
@@ -1,6 +0,0 @@
-$('#navbar-content').on('show.bs.collapse', () => {
-    $('.banner').css('margin-top', '0');
-});
-$('#navbar-content').on('hide.bs.collapse', () => {
-    $('.banner').css('margin-top', '-106px');
-});
